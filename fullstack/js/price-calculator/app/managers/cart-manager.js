ae6ffef4d11e6d255ca013750ce927f7cfcc7c35
Cosmin Ianculescu <1773842+cosmini@users.noreply.github.com>
2022-07-04 09:17:57 +1000

    fix: update arguments when creating cart item model

--- fullstack/js/price-calculator/app/managers/cart-manager.js
+++ fullstack/js/price-calculator/app/managers/cart-manager.js
@@ -14,7 +14,13 @@ export default class CartManager {
     const rawData = require(jsonFile)
 
     for (const rawItem in rawData) {
-      const cartItem = new CartItem(rawItem)
+      const cartItem = new CartItem(
+        rawItem['product-type'],
+        rawItem.options,
+        rawItem['artist-markup'],
+        rawItem.quantity,
+        this.basePriceManager
+      )
       data.push(cartItem)
     }
 
